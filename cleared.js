d10237b9555c60abfa695758de3ac1900c27b03f
re4ever <re4ever@naver.com>
2026-06-30 21:56:40 +0900

    클리어이미지 추가

---
+++ cleared.js
@@ -0,0 +1,950 @@
+// Cleared Image 매핑 (수동 편집용). 키 = 카드 이미지에서 확장자 뺀 id, 값 = 이미지 경로 배열.
+// 이 파일만 고치면 Cleared Image 유무/내용이 바뀝니다 (index.html 수정 불필요).
+window.CLEARED = {
+  "11100010": [
+    "cleared/cut10000101.png"
+  ],
+  "11100020": [
+    "cleared/cut10000201.png"
+  ],
+  "11100030": [
+    "cleared/cut10000301.png"
+  ],
+  "11100040": [
+    "cleared/cut10000401.png"
+  ],
+  "11100050": [
+    "cleared/cut10000501.png"
+  ],
+  "11100060": [
+    "cleared/cut10000601.png"
+  ],
+  "11100070": [
+    "cleared/cut10000701.png"
+  ],
+  "11100080": [
+    "cleared/cut10000801.png"
+  ],
+  "11100090": [
+    "cleared/cut10000901.png"
+  ],
+  "11100100": [
+    "cleared/cut10001001.png"
+  ],
+  "11100110": [
+    "cleared/cut10001101.png"
+  ],
+  "11100120": [
+    "cleared/cut10001201.png"
+  ],
+  "11100130": [
+    "cleared/cut10001301.png"
+  ],
+  "11100140": [
+    "cleared/cut10001401.png"
+  ],
+  "11100150": [
+    "cleared/cut10001501.png"
+  ],
+  "11100160": [
+    "cleared/cut10001601.png"
+  ],
+  "11100170": [
+    "cleared/cut10001701.png",
+    "cleared/cut10008101.png"
+  ],
+  "11100180": [
+    "cleared/cut10001801.png"
+  ],
+  "11100190": [
+    "cleared/cut10001901.png"
+  ],
+  "11100200": [
+    "cleared/cut10002001.png"
+  ],
+  "11100210": [
+    "cleared/cut10002101.png"
+  ],
+  "11100220": [
+    "cleared/cut10002201.png"
+  ],
+  "11100230": [
+    "cleared/cut10002301.png"
+  ],
+  "11100240": [
+    "cleared/cut10002401.png"
+  ],
+  "11100250": [
+    "cleared/cut10002501.png"
+  ],
+  "11100260": [
+    "cleared/cut10002601.png"
+  ],
+  "11100270": [
+    "cleared/cut10002701.png"
+  ],
+  "11100280": [
+    "cleared/cut10002801.png"
+  ],
+  "11100290": [
+    "cleared/cut10003001.png"
+  ],
+  "11100300": [
+    "cleared/cut10003101.png"
+  ],
+  "11100310": [
+    "cleared/cut10003201.png"
+  ],
+  "11100320": [
+    "cleared/cut10003301.png"
+  ],
+  "11100330": [
+    "cleared/cut10003401.png"
+  ],
+  "11100340": [
+    "cleared/cut10003501.png"
+  ],
+  "11100350": [
+    "cleared/cut10003601.png"
+  ],
+  "11200430": [
+    "cleared/cut10003701.png"
+  ],
+  "11200440": [
+    "cleared/cut10003801.png"
+  ],
+  "11200450": [
+    "cleared/cut10003901.png"
+  ],
+  "11200460": [
+    "cleared/cut10004001.png"
+  ],
+  "11200470": [
+    "cleared/cut10004101.png"
+  ],
+  "11200480": [
+    "cleared/cut10004201.png"
+  ],
+  "11200490": [
+    "cleared/cut10004301.png"
+  ],
+  "11200500": [
+    "cleared/cut10004401.png"
+  ],
+  "11200510": [
+    "cleared/cut10004501.png"
+  ],
+  "11200520": [
+    "cleared/cut10004601.png"
+  ],
+  "11200530": [
+    "cleared/cut10004701.png"
+  ],
+  "11200540": [
+    "cleared/cut10004801.png"
+  ],
+  "11200550": [
+    "cleared/cut10004901.png"
+  ],
+  "11200560": [
+    "cleared/cut10005001.png"
+  ],
+  "11200570": [
+    "cleared/cut10005101.png"
+  ],
+  "11200580": [
+    "cleared/cut10005201.png"
+  ],
+  "11200590": [
+    "cleared/cut10005301.png"
+  ],
+  "11200600": [
+    "cleared/cut10005401.png"
+  ],
+  "11200610": [
+    "cleared/cut10005501.png"
+  ],
+  "11200620": [
+    "cleared/cut10005601.png"
+  ],
+  "11200630": [
+    "cleared/cut10005701.png"
+  ],
+  "11200640": [
+    "cleared/cut10005801.png"
+  ],
+  "11200650": [
+    "cleared/cut10005901.png"
+  ],
+  "11200660": [
+    "cleared/cut10006001.png"
+  ],
+  "11200670": [
+    "cleared/cut10006101.png"
+  ],
+  "11200680": [
+    "cleared/cut10006201.png"
+  ],
+  "11200690": [
+    "cleared/cut10006301.png"
+  ],
+  "11200700": [
+    "cleared/cut10006401.png"
+  ],
+  "11200710": [
+    "cleared/cut10006501.png"
+  ],
+  "11200720": [
+    "cleared/cut10006601.png"
+  ],
+  "11200730": [
+    "cleared/cut10006801.png"
+  ],
+  "11200740": [
+    "cleared/cut10007001.png"
+  ],
+  "11200750": [
+    "cleared/cut10007201.png"
+  ],
+  "11200760": [
+    "cleared/cut10007301.png"
+  ],
+  "11200770": [
+    "cleared/cut10007401.png"
+  ],
+  "11200780": [
+    "cleared/cut10007501.png"
+  ],
+  "11300850": [
+    "cleared/cut10008201.png"
+  ],
+  "11300860": [
+    "cleared/cut10008301.png"
+  ],
+  "11300870": [
+    "cleared/cut10008401.png"
+  ],
+  "11300880": [
+    "cleared/cut10008501.png"
+  ],
+  "11300890": [
+    "cleared/cut10008601.png"
+  ],
+  "11300900": [
+    "cleared/cut10008701.png"
+  ],
+  "11300910": [
+    "cleared/cut10008801.png"
+  ],
+  "11300920": [
+    "cleared/cut10008901.png"
+  ],
+  "11300930": [
+    "cleared/cut10009001.png"
+  ],
+  "11300940": [
+    "cleared/cut10009101.png"
+  ],
+  "11300950": [
+    "cleared/cut10009201.png"
+  ],
+  "11300960": [
+    "cleared/cut10009301.png"
+  ],
+  "11300970": [
+    "cleared/cut10009401.png"
+  ],
+  "11300980": [
+    "cleared/cut10009501.png"
+  ],
+  "11300990": [
+    "cleared/cut10009601.png"
+  ],
+  "11301000": [
+    "cleared/cut10009701.png"
+  ],
+  "11301010": [
+    "cleared/cut10009801.png"
+  ],
+  "11301020": [
+    "cleared/cut10009901.png"
+  ],
+  "11301030": [
+    "cleared/cut10010001.png"
+  ],
+  "11301040": [
+    "cleared/cut10010101.png"
+  ],
+  "11301050": [
+    "cleared/cut10010201.png"
+  ],
+  "11301060": [
+    "cleared/cut10010301.png"
+  ],
+  "11301070": [
+    "cleared/cut10109601.png"
+  ],
+  "11301080": [
+    "cleared/cut10010401.png"
+  ],
+  "11301090": [
+    "cleared/cut10010501.png"
+  ],
+  "11301100": [
+    "cleared/cut10010601.png"
+  ],
+  "11301110": [
+    "cleared/cut10010701.png"
+  ],
+  "11301120": [
+    "cleared/cut10010801.png"
+  ],
+  "11301130": [
+    "cleared/cut10010901.png"
+  ],
+  "11301140": [
+    "cleared/cut10011001.png"
+  ],
+  "11301150": [
+    "cleared/cut10011101.png"
+  ],
+  "11301160": [
+    "cleared/cut10011201.png"
+  ],
+  "11301170": [
+    "cleared/cut10011301.png"
+  ],
+  "11301180": [
+    "cleared/cut10011401.png"
+  ],
+  "11301190": [
+    "cleared/cut10011501.png"
+  ],
+  "11301200": [
+    "cleared/cut10011601.png"
+  ],
+  "11301210": [
+    "cleared/cut10011701.png"
+  ],
+  "11301280": [
+    "cleared/cut10011801.png"
+  ],
+  "11401290": [
+    "cleared/cut10011901.png"
+  ],
+  "11401300": [
+    "cleared/cut10012001.png"
+  ],
+  "11401310": [
+    "cleared/cut10012101.png"
+  ],
+  "11401320": [
+    "cleared/cut10012201.png"
+  ],
+  "11401330": [
+    "cleared/cut10012301.png"
+  ],
+  "11401340": [
+    "cleared/cut10012401.png"
+  ],
+  "11401350": [
+    "cleared/cut10012501.png"
+  ],
+  "11401360": [
+    "cleared/cut10012601.png"
+  ],
+  "11401370": [
+    "cleared/cut10012701.png"
+  ],
+  "11401380": [
+    "cleared/cut10012801.png"
+  ],
+  "11401390": [
+    "cleared/cut10012901.png"
+  ],
+  "11401400": [
+    "cleared/cut10013001.png"
+  ],
+  "11401410": [
+    "cleared/cut10013101.png"
+  ],
+  "11401420": [
+    "cleared/cut10013201.png"
+  ],
+  "11401430": [
+    "cleared/cut10013301.png"
+  ],
+  "11401440": [
+    "cleared/cut10013401.png"
+  ],
+  "11401450": [
+    "cleared/cut10013501.png"
+  ],
+  "11401460": [
+    "cleared/cut10013601.png"
+  ],
+  "11401470": [
+    "cleared/cut10013701.png"
+  ],
+  "11401480": [
+    "cleared/cut10013801.png"
+  ],
+  "11401490": [
+    "cleared/cut10013901.png"
+  ],
+  "11401500": [
+    "cleared/cut10014001.png"
+  ],
+  "11401510": [
+    "cleared/cut10014101.png"
+  ],
+  "11401520": [
+    "cleared/cut10014201.png"
+  ],
+  "11401530": [
+    "cleared/cut10014301.png"
+  ],
+  "11401540": [
+    "cleared/cut10014401.png"
+  ],
+  "11401550": [
+    "cleared/cut10014501.png"
+  ],
+  "11401560": [
+    "cleared/cut10014601.png"
+  ],
+  "11401570": [
+    "cleared/cut10014701.png"
+  ],
+  "11401580": [
+    "cleared/cut10014801.png"
+  ],
+  "11401590": [
+    "cleared/cut10014901.png"
+  ],
+  "11401600": [
+    "cleared/cut10015001.png"
+  ],
+  "11401610": [
+    "cleared/cut10015101.png"
+  ],
+  "11401620": [
+    "cleared/cut10015201.png"
+  ],
+  "11401630": [
+    "cleared/cut10015301.png"
+  ],
+  "11401640": [
+    "cleared/cut10015401.png"
+  ],
+  "11401650": [
+    "cleared/cut10015501.png"
+  ],
+  "11401660": [
+    "cleared/cut10015601.png"
+  ],
+  "11401670": [
+    "cleared/cut10015701.png"
+  ],
+  "11401680": [
+    "cleared/cut10015801.png"
+  ],
+  "11401690": [
+    "cleared/cut10015901.png"
+  ],
+  "11401700": [
+    "cleared/cut10016001.png"
+  ],
+  "11401710": [
+    "cleared/cut10016101.png"
+  ],
+  "11401720": [
+    "cleared/cut10016201.png"
+  ],
+  "12101730": [
+    "cleared/cut10016301.png"
+  ],
+  "12101740": [
+    "cleared/cut10016401.png"
+  ],
+  "12101750": [
+    "cleared/cut10016501.png"
+  ],
+  "12101760": [
+    "cleared/cut10016601.png"
+  ],
+  "12101770": [
+    "cleared/cut10016701.png"
+  ],
+  "12101780": [
+    "cleared/cut10016801.png"
+  ],
+  "12101790": [
+    "cleared/cut10016901.png"
+  ],
+  "12101800": [
+    "cleared/cut10017001.png"
+  ],
+  "12101810": [
+    "cleared/cut10017101.png"
+  ],
+  "12101820": [
+    "cleared/cut10017201.png"
+  ],
+  "12101830": [
+    "cleared/cut10017301.png"
+  ],
+  "12101840": [
+    "cleared/cut10017401.png"
+  ],
+  "12101850": [
+    "cleared/cut10017501.png"
+  ],
+  "12101860": [
+    "cleared/cut10017601.png"
+  ],
+  "12101870": [
+    "cleared/cut10017701.png"
+  ],
+  "12101880": [
+    "cleared/cut10017801.png"
+  ],
+  "12101890": [
+    "cleared/cut10017901.png"
+  ],
+  "12101900": [
+    "cleared/cut10018001.png"
+  ],
+  "12101910": [
+    "cleared/cut10018101.png"
+  ],
+  "12101920": [
+    "cleared/cut10018201.png"
+  ],
+  "12101930": [
+    "cleared/cut10018301.png"
+  ],
+  "12101940": [
+    "cleared/cut10018401.png"
+  ],
+  "12101950": [
+    "cleared/cut10018501.png"
+  ],
+  "12101960": [
+    "cleared/cut10018601.png"
+  ],
+  "12101970": [
+    "cleared/cut10018701.png"
+  ],
+  "12101980": [
+    "cleared/cut10018801.png"
+  ],
+  "12101990": [
+    "cleared/cut10018901.png"
+  ],
+  "12102000": [
+    "cleared/cut10019001.png"
+  ],
+  "12102010": [
+    "cleared/cut10019101.png"
+  ],
+  "12102020": [
+    "cleared/cut10019201.png"
+  ],
+  "12102030": [
+    "cleared/cut10019301.png"
+  ],
+  "12102040": [
+    "cleared/cut10019401.png"
+  ],
+  "12102050": [
+    "cleared/cut10019501.png"
+  ],
+  "12102060": [
+    "cleared/cut10019601.png"
+  ],
+  "12102070": [
+    "cleared/cut10019701.png"
+  ],
+  "12102080": [
+    "cleared/cut10019801.png"
+  ],
+  "12102090": [
+    "cleared/cut10019901.png"
+  ],
+  "12202170": [
+    "cleared/cut10020001.png"
+  ],
+  "12202180": [
+    "cleared/cut10020101.png"
+  ],
+  "12202190": [
+    "cleared/cut10020201.png"
+  ],
+  "12202200": [
+    "cleared/cut10020301.png"
+  ],
+  "12202210": [
+    "cleared/cut10020401.png"
+  ],
+  "12202220": [
+    "cleared/cut10020501.png"
+  ],
+  "12202230": [
+    "cleared/cut10020601.png"
+  ],
+  "12202240": [
+    "cleared/cut10020701.png"
+  ],
+  "12202250": [
+    "cleared/cut10020801.png"
+  ],
+  "12202260": [
+    "cleared/cut10020901.png"
+  ],
+  "12202270": [
+    "cleared/cut10021001.png"
+  ],
+  "12202280": [
+    "cleared/cut10021101.png"
+  ],
+  "12202290": [
+    "cleared/cut10021201.png"
+  ],
+  "12202300": [
+    "cleared/cut10021301.png"
+  ],
+  "12202310": [
+    "cleared/cut10021401.png"
+  ],
+  "12202320": [
+    "cleared/cut10021501.png"
+  ],
+  "12202330": [
+    "cleared/cut10021601.png"
+  ],
+  "12202340": [
+    "cleared/cut10021701.png"
+  ],
+  "12202350": [
+    "cleared/cut10021801.png"
+  ],
+  "12202360": [
+    "cleared/cut10021901.png"
+  ],
+  "12202370": [
+    "cleared/cut10022001.png"
+  ],
+  "12202380": [
+    "cleared/cut10022101.png"
+  ],
+  "12202390": [
+    "cleared/cut10022201.png"
+  ],
+  "12202400": [
+    "cleared/cut10022301.png"
+  ],
+  "12202410": [
+    "cleared/cut10022401.png"
+  ],
+  "12202420": [
+    "cleared/cut10022501.png"
+  ],
+  "12202430": [
+    "cleared/cut10022601.png"
+  ],
+  "12202440": [
+    "cleared/cut10022701.png"
+  ],
+  "12202450": [
+    "cleared/cut10022801.png"
+  ],
+  "12202460": [
+    "cleared/cut10022901.png"
+  ],
+  "12202470": [
+    "cleared/cut10023001.png"
+  ],
+  "12202480": [
+    "cleared/cut10023101.png"
+  ],
+  "12202490": [
+    "cleared/cut10023201.png"
+  ],
+  "12202500": [
+    "cleared/cut10023301.png"
+  ],
+  "12202510": [
+    "cleared/cut10023401.png"
+  ],
+  "12202520": [
+    "cleared/cut10023501.png"
+  ],
+  "12202530": [
+    "cleared/cut10023601.png"
+  ],
+  "12202540": [
+    "cleared/cut10023701.png"
+  ],
+  "12202550": [
+    "cleared/cut10023801.png"
+  ],
+  "12202560": [
+    "cleared/cut10023901.png"
+  ],
+  "12302610": [
+    "cleared/cut10024001.png"
+  ],
+  "12302620": [
+    "cleared/cut10024101.png"
+  ],
+  "12302630": [
+    "cleared/cut10024201.png"
+  ],
+  "12302640": [
+    "cleared/cut10024301.png"
+  ],
+  "12302650": [
+    "cleared/cut10024401.png"
+  ],
+  "12302660": [
+    "cleared/cut10024501.png"
+  ],
+  "12302670": [
+    "cleared/cut10024601.png"
+  ],
+  "12302680": [
+    "cleared/cut10024701.png"
+  ],
+  "12302690": [
+    "cleared/cut10024801.png"
+  ],
+  "12302700": [
+    "cleared/cut10024901.png"
+  ],
+  "12302710": [
+    "cleared/cut10025001.png"
+  ],
+  "12302720": [
+    "cleared/cut10025101.png"
+  ],
+  "12302730": [
+    "cleared/cut10025201.png"
+  ],
+  "12302740": [
+    "cleared/cut10025301.png"
+  ],
+  "12302750": [
+    "cleared/cut10025401.png"
+  ],
+  "12302760": [
+    "cleared/cut10025501.png"
+  ],
+  "12302770": [
+    "cleared/cut10025601.png"
+  ],
+  "12302780": [
+    "cleared/cut10025701.png"
+  ],
+  "12302790": [
+    "cleared/cut10025801.png"
+  ],
+  "12302800": [
+    "cleared/cut10025901.png"
+  ],
+  "12302810": [
+    "cleared/cut10026001.png"
+  ],
+  "12302820": [
+    "cleared/cut10026101.png"
+  ],
+  "12302830": [
+    "cleared/cut10026201.png"
+  ],
+  "12302840": [
+    "cleared/cut10026301.png"
+  ],
+  "12302850": [
+    "cleared/cut10026401.png"
+  ],
+  "12302860": [
+    "cleared/cut10026501.png"
+  ],
+  "12302870": [
+    "cleared/cut10026601.png"
+  ],
+  "12302880": [
+    "cleared/cut10026701.png"
+  ],
+  "12302890": [
+    "cleared/cut10026801.png"
+  ],
+  "12302900": [
+    "cleared/cut10026901.png"
+  ],
+  "12302910": [
+    "cleared/cut10027001.png"
+  ],
+  "12302920": [
+    "cleared/cut10027101.png"
+  ],
+  "12302930": [
+    "cleared/cut10027201.png"
+  ],
+  "12302940": [
+    "cleared/cut10027301.png"
+  ],
+  "12302950": [
+    "cleared/cut10027401.png"
+  ],
+  "12302960": [
+    "cleared/cut10027501.png"
+  ],
+  "12302970": [
+    "cleared/cut10027601.png"
+  ],
+  "12302980": [
+    "cleared/cut10027701.png"
+  ],
+  "12302990": [
+    "cleared/cut10027801.png"
+  ],
+  "12303000": [
+    "cleared/cut10027901.png"
+  ],
+  "12403050": [
+    "cleared/cut10028001.png"
+  ],
+  "12403060": [
+    "cleared/cut10028101.png"
+  ],
+  "12403070": [
+    "cleared/cut10028201.png"
+  ],
+  "12403080": [
+    "cleared/cut10028301.png"
+  ],
+  "12403090": [
+    "cleared/cut10028401.png"
+  ],
+  "12403100": [
+    "cleared/cut10028501.png"
+  ],
+  "12403110": [
+    "cleared/cut10028601.png"
+  ],
+  "12403120": [
+    "cleared/cut10028701.png"
+  ],
+  "12403130": [
+    "cleared/cut10028801.png"
+  ],
+  "12403140": [
+    "cleared/cut10028901.png"
+  ],
+  "12403150": [
+    "cleared/cut10029001.png"
+  ],
+  "12403160": [
+    "cleared/cut10029101.png"
+  ],
+  "12403170": [
+    "cleared/cut10029201.png"
+  ],
+  "12403180": [
+    "cleared/cut10029301.png"
+  ],
+  "12403190": [
+    "cleared/cut10029401.png"
+  ],
+  "12403200": [
+    "cleared/cut10029501.png"
+  ],
+  "12403210": [
+    "cleared/cut10029601.png"
+  ],
+  "12403220": [
+    "cleared/cut10029701.png"
+  ],
+  "12403230": [
+    "cleared/cut10029801.png"
+  ],
+  "12403240": [
+    "cleared/cut10029901.png"
+  ],
+  "12403250": [
+    "cleared/cut10030001.png"
+  ],
+  "12403260": [
+    "cleared/cut10030101.png"
+  ],
+  "12403270": [
+    "cleared/cut10030201.png"
+  ],
+  "12403280": [
+    "cleared/cut10030301.png"
+  ],
+  "12403290": [
+    "cleared/cut10030401.png"
+  ],
+  "12403300": [
+    "cleared/cut10030501.png"
+  ],
+  "12403310": [
+    "cleared/cut10030601.png"
+  ],
+  "12403320": [
+    "cleared/cut10030701.png"
+  ],
+  "12403330": [
+    "cleared/cut10030801.png"
+  ],
+  "12403340": [
+    "cleared/cut10030901.png"
+  ],
+  "12403350": [
+    "cleared/cut10031001.png"
+  ],
+  "12403360": [
+    "cleared/cut10031101.png"
+  ],
+  "12403370": [
+    "cleared/cut10031201.png"
+  ],
+  "12403380": [
+    "cleared/cut10031301.png"
+  ],
+  "12403390": [
+    "cleared/cut10031401.png"
+  ],
+  "12403400": [
+    "cleared/cut10031501.png"
+  ],
+  "12403410": [
+    "cleared/cut10031601.png"
+  ],
+  "12403420": [
+    "cleared/cut10031701.png"
+  ],
+  "12403430": [
+    "cleared/cut10031801.png"
+  ],
+  "12403440": [
+    "cleared/cut10031901.png"
+  ],
+  "12403450": [
+    "cleared/cut10032001.png"
+  ],
+  "12403460": [
+    "cleared/cut10032101.png"
+  ],
+  "12403470": [
+    "cleared/cut10032201.png"
+  ],
+  "12403480": [
+    "cleared/cut10032301.png"
+  ],
+  "12403490": [
+    "cleared/cut10032401.png"
+  ]
+};
